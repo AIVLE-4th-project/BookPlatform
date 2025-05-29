384fcdabc9d0927d17dc2d29bc4bd009d2da90cd
changeme4585 <benscience@naver.com>
2025-05-29 14:04:10 +0900

    상세 정보를 조회하는 컨트롤러

--- src/main/java/com/example/BookPlatform/controller/BookController.java
+++ src/main/java/com/example/BookPlatform/controller/BookController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,5 +16,11 @@ public class BookController {
     @GetMapping(value = "/book-list")
     public ResponseEntity<?>  getBookList() {
         return new ResponseEntity<>(bookService.getBookList(), HttpStatus.OK);
+    }
+
+
+    @GetMapping(value = "/book-detail-info")
+    public ResponseEntity<?> getBookDetailInfo( @RequestParam("id")Long id) {
+        return new ResponseEntity<>(bookService.getBookDetailInfo(id), HttpStatus.OK);
     }
 }
