3047bdef8772dffb7f9457436cadfe2be4305e40
changeme4585 <benscience@naver.com>
2025-05-29 13:37:42 +0900

    Book 객체를 DTO로 변환 후 list로 반환

--- src/main/java/com/example/BookPlatform/service/BookService.java
+++ src/main/java/com/example/BookPlatform/service/BookService.java
@@ -1,12 +1,31 @@
 package com.example.BookPlatform.service;
 
 
+import com.example.BookPlatform.dto.response.BookInfoDto;
+import com.example.BookPlatform.entity.Book;
+import com.example.BookPlatform.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class BookService {
+    private final BookRepository bookRepository;
+    public List<BookInfoDto> getBookList(){
+        List<Book> bookList = bookRepository.findAll();
+        List<BookInfoDto> bookInfoDtoList = new ArrayList<>();
+        for (Book book: bookList){
+            BookInfoDto bookInfoDto = BookInfoDto.builder().
+                    title(book.getTitle()).
+                    content(book.getContent()).
+                    build();
+            bookInfoDtoList.add(bookInfoDto);
+        }
+        return bookInfoDtoList;
+    }
 }
