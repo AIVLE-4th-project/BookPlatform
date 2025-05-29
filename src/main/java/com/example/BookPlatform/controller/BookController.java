5cb419f598c5cd7eaf47926aeff6a6942178d751
changeme4585 <benscience@naver.com>
2025-05-29 13:38:07 +0900

    Book 객체 list를 조회하는 컨트롤러

--- src/main/java/com/example/BookPlatform/controller/BookController.java
+++ src/main/java/com/example/BookPlatform/controller/BookController.java
@@ -1,9 +1,19 @@
 package com.example.BookPlatform.controller;
 
+import com.example.BookPlatform.service.BookService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class BookController {
+    private  final BookService bookService;
+
+    @GetMapping(value = "/book-list")
+    public ResponseEntity<?>  getBookList() {
+        return new ResponseEntity<>(bookService.getBookList(), HttpStatus.OK);
+    }
 }
