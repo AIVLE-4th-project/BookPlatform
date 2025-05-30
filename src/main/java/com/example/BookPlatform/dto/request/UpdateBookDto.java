c140f4fbabafc02715eed958245875242e4f9c96
changeme4585 <benscience@naver.com>
2025-05-30 11:43:09 +0900

    feat : update DTO

---
+++ src/main/java/com/example/BookPlatform/dto/request/UpdateBookDto.java
@@ -0,0 +1,25 @@
+package com.example.BookPlatform.dto.request;
+
+
+import jakarta.persistence.Column;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class UpdateBookDto {
+    @Column
+    private Long id; //DB 기본키
+    @Column
+    private  String title; //제목
+    @Column
+    private  String content; //본문
+
+    @Column
+    private String author; //저자
+
+    @Column
+    private String coverUrl; //이미지 url
+
+
+}
