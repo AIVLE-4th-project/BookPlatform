5da01acfb6efcf520bc898d29ab8fddd19f2dc07
changeme4585 <benscience@naver.com>
2025-05-30 10:07:11 +0900

    feat : 저자 필드 추가 및 url 필드명 수정

--- src/main/java/com/example/BookPlatform/dto/request/SaveBookInfoDto.java
+++ src/main/java/com/example/BookPlatform/dto/request/SaveBookInfoDto.java
@@ -1,14 +1,22 @@
 package com.example.BookPlatform.dto.request;
 
 import jakarta.persistence.Column;
+import lombok.Builder;
+import lombok.Getter;
 
-//책 정보 등록 ㅇ썌
+//책 정보 등록
+@Getter
+@Builder
 public class SaveBookInfoDto {
 
     @Column
     private  String title; //제목
     @Column
     private  String content; //본문
+
     @Column
-    private String imageUrl; //이미지 url
+    private String author; //저자
+
+    @Column
+    private String coverUrl; //이미지 url
 }
